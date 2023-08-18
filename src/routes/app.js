32c80097ea1ee3d13c6b026071f7dbe43f1093d4
fajarnursodiqq <fajarnursodiqq@gmail.com>
2023-08-18 21:11:47 +0700

    testing

---
+++ src/routes/app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const apiRoutes = require("./apiRoutes");
+const Redis = require("ioredis");
+
+const app = express();
+const port = 3000;
+
+const redisClient = new Redis();
+
+app.use(bodyParser.json());
+
+app.use("/api", apiRoutes);
+
+redisClient.on("connect", () => {
+  console.log("Connected to Redis");
+});
+
+const db = require("../models/db");
+db.connect((err) => {
+  if (err) {
+    console.error("Error connecting to MySQL: " + err.stack);
+    return;
+  }
+  console.log("Connected to MySQL as id " + db.threadId);
+});
+
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`);
+});
