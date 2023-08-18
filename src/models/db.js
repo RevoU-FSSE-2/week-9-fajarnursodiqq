e73c234b43dc93dbfdd9215f5db3c0f18dc3742c
fajarnursodiqq <fajarnursodiqq@gmail.com>
2023-08-19 01:44:16 +0700

    membuat data di railway

--- src/models/db.js
+++ src/models/db.js
@@ -1,10 +1,18 @@
 const mysql = require("mysql");
 
+// const db = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   password: "Amsterdam2021",
+//   database: "w9",
+// });
+
 const db = mysql.createConnection({
-  host: "localhost",
+  host: "containers-us-west-154.railway.app",
+  //port: "7527",
   user: "root",
-  password: "Amsterdam2021",
-  database: "w9",
+  password: "1q18KXxgh0XVoIVBsqlf",
+  database: "railway",
 });
 
 module.exports = db;
